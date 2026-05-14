dadca344e727daae1dfd3035a126c998fb929692
lironldn <liron.schur@red-badger.com>
2026-05-14 11:08:18 +0100

    Initial skeleton api

---
+++ FindMyHobby/FindMyHobbyApi/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var hobbies = new[]
+{
+    "Hiking", "Bouldering", "Karaoke", "Pottery", "Cooking", "Drawing", "Salsa dancing", "Art Galleries", "Cinema", "Theatre"
+};
+
+app.MapGet("/hobby", () =>
+    {
+        var hobby = Enumerable.Range(1, 5).Select(index =>
+                new Hobby
+                (
+                    hobbies[Random.Shared.Next(hobbies.Length)]
+                ))
+            .ToArray();
+        return hobby;
+    })
+    .WithName("GetHobby");
+
+app.Run();
+
+record Hobby(string Name)
+{
+    //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
